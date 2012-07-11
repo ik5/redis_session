50ef83f15d41f1a8586a81a070a823f340b81f54
ik5 <idok@linesip.com>
2012-07-11 15:47:07 +0300

    Wrote the first test

--- specs/add_restore_spec.rb
+++ specs/add_restore_spec.rb
@@ -1,7 +1,13 @@
 require 'redis_session'
 
-describe Session do
+describe Session::SessionClient do
   before do
-   
+   @session = Session::SessionClient.new(:prefix => 'add_test')
+  end
+
+  context 'With no values' do
+    it 'should return empty hash' do
+      @session.restore('name').length.should equal(0)
+    end
   end
 end
