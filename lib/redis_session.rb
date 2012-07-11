303462b259507a4bc4a4762b45c0ab7b27f67af3
ik5 <idok@linesip.com>
2012-07-11 16:22:25 +0300

    Fixed the Marshal from dump to load

--- lib/redis_session.rb
+++ lib/redis_session.rb
@@ -37,7 +37,7 @@ module Session
     def restore(key)
       a_key = "#{@options[:prefix]}#{key}"
       data  = @redis.get(a_key)
-      data  = {} if data.nil?
+      data.nil? ? {} : Marshal.load(data)
     rescue
       {}
     end
